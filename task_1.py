81afedb49a05eb82a2f0e41c35dbae7447dfb9d6
eyakovleva <ekaterinajakovl8@gmail.com>
2020-10-03 14:05:05 +0300

    Did the 1st task for a lesson 2

---
+++ task_1.py
@@ -0,0 +1,7 @@
+x = ["DarkTower", 123, 12.456, ("horse", 2), {"book", "snow", "hiking"}, {"Lord of the flies": "Golding",
+     "The old man and the sea": "Hemingway", "Martin Eden": "Jack London"}, ["apple", "banana", 'potato']]
+
+
+for i in x:
+    print(type(i))
+
